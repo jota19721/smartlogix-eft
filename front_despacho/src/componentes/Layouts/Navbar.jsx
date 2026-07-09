1be472a1ed8f06256b360a9438e3ef9f2d5ef9cf
jota19721 <j.palacios@duocuc.cl>
2026-07-09 10:28:51 -0400

    test: cambiar navbar para demostrar pipeline

--- front_despacho/src/componentes/Layouts/Navbar.jsx
+++ front_despacho/src/componentes/Layouts/Navbar.jsx
@@ -1,15 +1,15 @@
 function Navbar() {
   return (
-    <nav className="rounded-xl w-[250px] min-h-[880px] bg-teal-600 text-white sticky top-0 p-4 m-4">
+    <nav className="rounded-xl w-[250px] min-h-[880px] bg-purple-700 text-white sticky top-0 p-4 m-4">
       {/* Logo o título */}
-      <h2 className="text-xl font-bold mb-8">Despacho Dashboard</h2>
+      <h2 className="text-xl font-bold mb-8">SmartLogix Demo CI/CD</h2>
 
       {/* Menú de navegación */}
       <ul className="space-y-3">
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            className="block font-bold py-2 px-3 hover:bg-purple-800 rounded"
           >
             Usuarios
           </a>
@@ -17,7 +17,7 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            className="block font-bold py-2 px-3 hover:bg-purple-800 rounded"
           >
             Productos
           </a>
@@ -25,7 +25,7 @@ function Navbar() {
         <li>
           <a
             href="#"
-            className="block font-bold py-2 px-3 hover:bg-teal-700 rounded"
+            className="block font-bold py-2 px-3 hover:bg-purple-800 rounded"
           >
             Configuración
           </a>
